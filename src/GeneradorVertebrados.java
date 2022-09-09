b43387a82ea9788a112c515f51e9cddc918395d7
stevenT9705 <torressteven9705@gmail.com>
2022-09-08 20:15:48 -0500

    prueba 1

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -1,8 +1,8 @@
 public class GeneradorVertebrados {
     public static Vertebrado[] generarArrayVertebrados(){
         Vertebrado[] miVertebrados = new Vertebrado[4];
-        miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
-        miVertebrados[1] = new Vertebrado("Vaca", "Mamifero");
+        miVertebrados[0] = new Vertebrado("camello", "Mamifero"); 
+        miVertebrados[1] = new Vertebrado("vaca", "Mamifero");
         miVertebrados[2] = new Vertebrado("Tiburón", "Pez"); 
         
         return miVertebrados;
